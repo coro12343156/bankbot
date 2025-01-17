a2544957d0deaecddb61d731d703d12e4f35bd82
coro <95575239+coro12343156@users.noreply.github.com>
2025-01-17 21:42:51 +0900

    pingコマンドを追加

---
+++ cogs/ping.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+from discord import app_commands
+import discord
+
+
+# commands.Cogを継承する
+class ping(commands.Cog): #好きな名前でOK(機能がわかる名前にすると良い)
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    # イベントリスナー(ボットが起動したときやメッセージを受信したとき等)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Cog ready!")
+	
+    # コマンドデコレーター(descriptionで説明が書ける)
+    @app_commands.command(name="ping", description="pingします")
+    async def ping(self,interaction:discord.Interaction):
+
+        await interaction.response.defer(ephemeral=True, thinking=True)
+
+        await interaction.followup.send("pong!")
+
+    # コマンドのエラーをprintするイベント
+    @ping.error #ここの名前も変える
+    async def raise_error(self, ctx, error):
+        print(error)
+
+async def setup(bot):
+    await bot.add_cog(ping(bot)) #ここの名前も変える
